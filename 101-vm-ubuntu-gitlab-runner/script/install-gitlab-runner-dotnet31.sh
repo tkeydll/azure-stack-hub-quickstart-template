56c5c02003cb49cb6e517f8e1a754a2b4fcf2413
ta <tkey.dll@gmail.com>
2020-11-04 10:19:11 +0900

    Create install-gitlab-runner-dotnet31.sh

---
+++ 101-vm-ubuntu-gitlab-runner/script/install-gitlab-runner-dotnet31.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+GITLAB_SERVER=$1
+GITLAB_TOKEN=$2
+
+echo "GITLAB_SERVER = " $GITLAB_SERVER
+echo "GITLAB_TOKEN = " $GITLAB_TOKEN
+
+# Install dotnet core 3.1
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update; \
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get update && \
+sudo apt-get install -y dotnet-sdk-3.1
+
+# Download and install gitlab-runner.
+curl -LJO https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb
+dpkg -i gitlab-runner_amd64.deb
+
+# Register
+sudo gitlab-runner register \
+  --non-interactive \
+  --url "$GITLAB_SERVER" \
+  --registration-token "$GITLAB_TOKEN" \
+  --executor "shell" \
+  --description "ubuntu shell runner" \
+  --tag-list "ubuntu, shell" \
+  --run-untagged="true" \
+  --locked="false" \
+  --access-level="not_protected"
