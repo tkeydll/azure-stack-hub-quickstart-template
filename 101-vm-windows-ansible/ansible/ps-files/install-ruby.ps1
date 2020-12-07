860e1e20c0539ac69281add7fe3875d70ed4e939
_ta <tkey.dll@gmail.com>
2020-12-08 00:35:37 +0900

    update install-ruby.ps1

--- 101-vm-windows-ansible/ansible/ps-files/install-ruby.ps1
+++ 101-vm-windows-ansible/ansible/ps-files/install-ruby.ps1
@@ -6,5 +6,9 @@ Invoke-WebRequest -Uri $url -OutFile $exe
 
 Start-Process -FilePath "$exe" -ArgumentList "/verysilent /tasks=modpath /lang=ja" -Wait
 
+# install ridk
+cmd /c ridk install 1 2 3
+cmd /c ridk exec pacman -S mingw-w64-x86_64-dlfcn --noconfirm
+
 # install rails
 cmd /c gem install rails
